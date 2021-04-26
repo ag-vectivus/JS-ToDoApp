5994d8358d22e55b8b19d2cc84ce6973bc1cb272
ag-vectivus <a.s.galecki@gmail.com>
2021-04-26 20:09:38 +0200

    b - Fix up bug, add remove button to generated todos

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const generateTemplate = (todo) => {
 	const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center todos__todo">
       ${todo}
+      <i class="far fa-trash-alt delete"></i>
     </li>
   `;
 
