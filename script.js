2d859e78594999d184f515728bbdee1b60e5c656
ag-vectivus <a.s.galecki@gmail.com>
2021-04-26 19:19:46 +0200

    Adding to do handler

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const todoList = document.querySelector(".todos");
+const addTodo = document.querySelector(".add");
+
+const generateTemplate = (todo) => {
+	const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center todos__todo">
+      ${todo}
+    </li>
+  `;
+
+	todoList.innerHTML += html;
+};
+
+addTodo.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const todo = addTodo.add.value.trim();
+
+	if (todo.length) {
+		generateTemplate(todo);
+		addTodo.reset();
+	}
+});
